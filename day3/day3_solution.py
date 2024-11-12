9ae37e39d5a6d9fc35b6ccb405f40ecaac9f08ca
Kate Williams <kate.williams109@nhs.net>
2024-11-12 11:20:59 +0000

    day3 completed! np.pad made part 3 very easy

--- day3/day3_solution.py
+++ day3/day3_solution.py
@@ -1,8 +1,9 @@
 import numpy as np
 from copy import deepcopy
+from typing import Callable
 
 
-def read_input_p1(file_name: str) -> np.ndarray:
+def read_input(file_name: str) -> np.ndarray:
     with open(file_name) as raw_image:
         grid = np.array(
             [
@@ -10,10 +11,10 @@ def read_input_p1(file_name: str) -> np.ndarray:
                 for line in raw_image.readlines()
             ]
         )
-    return grid.astype(int)
+    return np.pad(grid.astype(int), pad_width=2, mode="constant", constant_values=0)
 
 
-def get_neighbours(grid: np.ndarray, row: int, column: int) -> list[str]:
+def get_neighbours_p1(grid: np.ndarray, row: int, column: int) -> list[str]:
     surrounding_coords = [
         (row - 1, column),
         (row, column - 1),
@@ -23,26 +24,48 @@ def get_neighbours(grid: np.ndarray, row: int, column: int) -> list[str]:
     return [grid[coord] for coord in surrounding_coords]
 
 
-def calculate_layer(grid: np.ndarray, layer_number: int) -> np.ndarray:
+def get_neighbours_p3(grid: np.ndarray, row: int, column: int) -> list[str]:
+    max_row, max_column = grid.shape
+    surrounding_coords = [
+        (row - 1, column - 1),
+        (row - 1, column),
+        (row - 1, column + 1),
+        (row, column - 1),
+        (row, column + 1),
+        (row + 1, column - 1),
+        (row + 1, column),
+        (row + 1, column + 1),
+    ]
+    return [
+        grid[(row, column)]
+        for row, column in surrounding_coords
+        if row < max_row and column < max_column and row > 0 and column > 0
+    ]
+
+
+def calculate_layer(
+    grid: np.ndarray, layer_number: int, neighbour_func: Callable
+) -> np.ndarray:
     new_layer = False
     updated_grid = deepcopy(grid)
     rows, columns = np.where(grid == layer_number)
     for row, column in zip(rows, columns):
         if all(
-            layer_number == int(neighbour)
-            for neighbour in get_neighbours(grid, row, column)
+            layer_number == neighbour for neighbour in neighbour_func(grid, row, column)
         ):
             updated_grid[row, column] = layer_number + 1
             new_layer = True
     return updated_grid, new_layer
 
 
-def calculate_all_layers(file_name: str) -> int:
-    current_grid = read_input_p1(file_name)
+def calculate_all_layers(file_name: str, neighbour_func: Callable) -> int:
+    current_grid = read_input(file_name)
     more_layers = True
     layer_number = 1
     while more_layers == True:
-        current_grid, more_layers = calculate_layer(current_grid, layer_number)
+        current_grid, more_layers = calculate_layer(
+            current_grid, layer_number, neighbour_func
+        )
         layer_number += 1
     return np.sum(current_grid)
 
@@ -50,5 +73,18 @@ def calculate_all_layers(file_name: str) -> int:
 if __name__ == "__main__":
     # part 1
 
-    print(calculate_all_layers("day3/day3_inputs/day3_practise_p1.txt"))
-    print(calculate_all_layers("day3/day3_inputs/day3_final_p1.txt"))
+    print(
+        calculate_all_layers("day3/day3_inputs/day3_practise_p1.txt", get_neighbours_p1)
+    )
+    print(calculate_all_layers("day3/day3_inputs/day3_final_p1.txt", get_neighbours_p1))
+
+    # part 2
+
+    print(calculate_all_layers("day3/day3_inputs/day3_final_p2.txt", get_neighbours_p1))
+
+    # part 3
+
+    print(
+        calculate_all_layers("day3/day3_inputs/day3_practise_p1.txt", get_neighbours_p3)
+    )
+    print(calculate_all_layers("day3/day3_inputs/day3_final_p3.txt", get_neighbours_p3))
