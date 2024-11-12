08d0b7d05dbe4ad024b0aec0529e41cd2906a8c9
Kate Williams <kate.williams109@nhs.net>
2024-11-12 11:40:32 +0000

    speedy day4!

---
+++ day4/day4_solution.py
@@ -0,0 +1,38 @@
+from statistics import median
+
+
+def find_total_strikes_p1(file_name: str) -> int:
+    with open(file_name) as raw_nails:
+        nail_heights = [int(nail.strip()) for nail in raw_nails.readlines()]
+    strike_count = 0
+    shortest_nail = min(nail_heights)
+    for nail in nail_heights:
+        strike_count += nail - shortest_nail
+    return strike_count
+
+
+def find_total_strikes_p3(file_name: str) -> int:
+    with open(file_name) as raw_nails:
+        nail_heights = [int(nail.strip()) for nail in raw_nails.readlines()]
+    strike_count = 0
+    median_nail = median(nail_heights)
+    print(median_nail)
+    for nail in nail_heights:
+        strike_count += abs(nail - median_nail)
+    return strike_count
+
+
+if __name__ == "__main__":
+    # part 1
+
+    print(find_total_strikes_p1("day4/day4_inputs/day4_practise_p1.txt"))
+    print(find_total_strikes_p1("day4/day4_inputs/day4_final_p1.txt"))
+
+    # part 2
+
+    print(find_total_strikes_p1("day4/day4_inputs/day4_final_p2.txt"))
+
+    # part 3
+
+    print(find_total_strikes_p3("day4/day4_inputs/day4_practise_p3.txt"))
+    print(find_total_strikes_p3("day4/day4_inputs/day4_final_p3.txt"))
