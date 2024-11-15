2f7985cb122fabf749b2520c4f0255c846cddcad
Kate Williams <kate.williams109@nhs.net>
2024-11-15 10:34:07 +0000

    committing this before I inevitable ruin the function

---
+++ day9/day9_solution.py
@@ -0,0 +1,27 @@
+STAMPS_P1 = [1, 3, 5, 10]
+STAMPS_P2 = [1, 3, 5, 10, 15, 16, 20, 24, 25, 30]
+
+def find_min_beetles(input_file: str, stamps: list[int]) -> int:
+    with open(input_file) as notes:
+        sparkballs = [int(line.strip()) for line in notes.readlines()]
+    beetle_count = 0
+    stamps.sort(reverse=True)
+    for sparkball in sparkballs:
+        for stamp in stamps:
+            if (no_beetles := sparkball // stamp) > 0:
+                beetle_count += no_beetles
+                sparkball %= stamp
+    return beetle_count
+
+if __name__ == "__main__":
+    
+    # part 1
+    
+    print(find_min_beetles("day9/day9_inputs/day9_practise_p1.txt", STAMPS_P1))
+    print(find_min_beetles("day9/day9_inputs/day9_final_p1.txt", STAMPS_P1))
+    
+    # part 2
+    
+    print(find_min_beetles("day9/day9_inputs/day9_practise_p2.txt", STAMPS_P2))
+    # print(find_min_beetles("day9/day9_inputs/day9_final_p1.txt", STAMPS_P1))
+    
