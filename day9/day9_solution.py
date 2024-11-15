96fbe932fb703e5a1fd382c1e5ab8947800ff8e2
Kate Williams <kate.williams109@nhs.net>
2024-11-15 10:55:59 +0000

    Revert "committing this before I inevitable ruin the function"
    This reverts commit 2f7985cb122fabf749b2520c4f0255c846cddcad.

--- day9/day9_solution.py
+++ day9/day9_solution.py
@@ -1,6 +1,3 @@
-from itertools import combinations_with_replacement
-from copy import deepcopy
-
 STAMPS_P1 = [1, 3, 5, 10]
 STAMPS_P2 = [1, 3, 5, 10, 15, 16, 20, 24, 25, 30]
 
@@ -10,24 +7,10 @@ def find_min_beetles(input_file: str, stamps: list[int]) -> int:
     beetle_count = 0
     stamps.sort(reverse=True)
     for sparkball in sparkballs:
-        max_beetles = 0
-        decreasing_sparkball = deepcopy(sparkball)
         for stamp in stamps:
             if (no_beetles := sparkball // stamp) > 0:
-                max_beetles += no_beetles
-                decreasing_sparkball %= stamp
-        estimated_min = sparkball // stamps[0]
-        found_smaller = False
-        for comb_size in range(estimated_min, max_beetles):
-            for potential_combination in combinations_with_replacement(stamps, comb_size):
-                if sum(potential_combination) == sparkball:
-                    found_smaller = True
-                    break
-            break
-        if found_smaller:
-            beetle_count += comb_size
-        else:
-            beetle_count += max_beetles
+                beetle_count += no_beetles
+                sparkball %= stamp
     return beetle_count
 
 if __name__ == "__main__":
